b2f7029942424cd8cc0ef6899a4a83fdff62e069
mrfantest2 <100039304+mrfantest2@users.noreply.github.com>
2026-09-12 19:23:16 +0400

    chore(localwhisper): bump v0.1.2

--- localwhisper-src/LocalWhisper/app/build.gradle.kts
+++ localwhisper-src/LocalWhisper/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "win.fantest.localwhisper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
